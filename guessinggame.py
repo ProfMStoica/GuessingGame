e17000bdd28841a28aaa0732f3473705622746cb
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-11-04 12:01:41 -0500

    Make a plan for implementing the guessing game.

--- guessinggame.py
+++ guessinggame.py
@@ -13,7 +13,17 @@ class GuessingGame:
         self.moe = Player("Moe")
 
     def start(self):
+        #pick a number to be guessed between  0 and 9
+
+        #repeat asking the user to play for each round of the game until someone wins
         pass
 
     def determineWinner(self):
+        #check if larry's guess matched the answer
+
+        #check if curly's guess matched the answer
+
+        #checxk if moe's guess matched the answer
+
+        #return the winning player if one guessed correctly or None
         return "TBD"
